2eb3d7c2285926526929e4f3745b47261b37f499
Ariel Sanchez <freska_96@hotmail.com>
2024-06-23 19:05:52 -0600

    proyecto finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import 'fomantic-ui-css/semantic.min.css';
+import Container from './components/Container';
+import Header from './components/Header';
+import InputTask from './components/InputTask';
+import TaskContent from './components/TaskContent';
 
 function App() {
+  // Inicializar initialTasks como un array vacío
+  let initialTasks = [];
+
+  // Intentar parsear el valor del localStorage
+  try {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      initialTasks = JSON.parse(storedTasks);
+    }
+  } catch (e) {
+    console.error('Error parsing JSON from localStorage:', e);
+  }
+
+  const [tasks, setTasks] = useState(initialTasks);
+
+  // Guardar tareas en el localStorage cuando cambien
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const createTask = (task) => {
+    setTasks([...tasks, task]);
+  };
+
+  const deleteTask = (id) => {
+    const currentTask = tasks.filter((task) => task.idTask !== id)
+    setTasks(currentTask);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+      <InputTask createTask={createTask} />
+      <TaskContent tasks={tasks} deleteTask={deleteTask} />
+    </Container>
   );
 }
 
